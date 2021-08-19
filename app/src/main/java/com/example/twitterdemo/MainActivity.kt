16f1f812312cd80eda1cb20969078ebccca2f871
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2021-08-19 19:14:11 +0200

    add username, password and sign up/login to Login layout

--- app/src/main/java/com/example/twitterdemo/MainActivity.kt
+++ app/src/main/java/com/example/twitterdemo/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.twitterdemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val startSignUpPage = Intent(applicationContext, SignUpLoginActivity::class.java)
+            startSignUpPage.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startSignUpPage.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(startSignUpPage)
+        }, 2000)
     }
 }
