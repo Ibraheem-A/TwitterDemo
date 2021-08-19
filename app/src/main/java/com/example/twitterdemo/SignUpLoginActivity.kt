4bd8ed6218e38ef0c9079b8751979325ab6e45e4
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2021-08-19 19:00:57 +0200

    add SignUpLoginActivity

---
+++ app/src/main/java/com/example/twitterdemo/SignUpLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.twitterdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_login)
+    }
+}
