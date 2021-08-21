831e5bf2ca88ac5b4fc2c717da1ce4fdd925fed1
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2021-08-21 18:15:58 +0200

    implement signUp behaviour

--- app/src/main/java/com/example/twitterdemo/SignUpLoginActivity.kt
+++ app/src/main/java/com/example/twitterdemo/SignUpLoginActivity.kt
@@ -3,8 +3,10 @@ package com.example.twitterdemo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.Toast
 import com.parse.ParseUser
 
 class SignUpLoginActivity : AppCompatActivity() {
@@ -27,7 +29,13 @@ class SignUpLoginActivity : AppCompatActivity() {
             if(ParseUtil.accountAlreadyExists(usernameInput, passwordInput)){
                 // Login
             } else {
-                // SignUp
+                val signUpResponse = UserSignUp.signUp(usernameInput, passwordInput)
+                if (signUpResponse[0].equals("true")){
+                    Toast.makeText(this, "Signed Up successfully", Toast.LENGTH_LONG).show();
+                    // Then Log User in
+                } else{
+                    Toast.makeText(this, signUpResponse[1], Toast.LENGTH_LONG).show();
+                }
             }
         }
 
