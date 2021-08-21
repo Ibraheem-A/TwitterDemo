04140d0bbf1e1d2c2e91a90340da14801d581e3e
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2021-08-21 21:26:14 +0200

    added .java to class

--- app/src/main/java/com/example/twitterdemo/SignUpLoginActivity.kt
+++ app/src/main/java/com/example/twitterdemo/SignUpLoginActivity.kt
@@ -44,7 +44,7 @@ class SignUpLoginActivity : AppCompatActivity() {
 
         if(loginResponse[0].equals(true)){
             Toast.makeText(this@SignUpLoginActivity, "Logged in successfully", Toast.LENGTH_LONG).show()
-            val startUsersListActivity = Intent(applicationContext, UserListActivity::class)
+            val startUsersListActivity = Intent(applicationContext, UserListActivity::class.java)
             startActivity(startUsersListActivity)
         } else {
             Toast.makeText(this@SignUpLoginActivity, loginResponse[1], Toast.LENGTH_LONG).show();
