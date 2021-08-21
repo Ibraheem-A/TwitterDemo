2eb58d81dcabc17b0ad20389faee3859aa0da96a
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2021-08-21 18:03:19 +0200

    add SignUp method

--- app/src/main/java/com/example/twitterdemo/UserSignUp.java
+++ app/src/main/java/com/example/twitterdemo/UserSignUp.java
@@ -1,4 +1,35 @@
 package com.example.twitterdemo;
 
+import android.util.Log;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
 public class UserSignUp {
+    String signUpStatus = "false";
+    String message = "Sign Up Failed";
+
+    public String[] signUp(String username, String password){
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null){
+                    Log.i("Signed up...", "Successfully");
+                    signUpStatus = "true";
+                } else {
+                    Log.i("Sign up...", "Failed " + e.getMessage());
+                    signUpStatus = "false";
+                }
+                message = e.getMessage();
+            }
+        });
+        return new String[] {signUpStatus, message};
+    }
+
+
 }
