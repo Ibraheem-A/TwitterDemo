68d86482e1abc8577675c7994889286ee9731ad7
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2021-08-21 19:04:29 +0200

    add login method

--- app/src/main/java/com/example/twitterdemo/UserLogin.java
+++ app/src/main/java/com/example/twitterdemo/UserLogin.java
@@ -1,4 +1,34 @@
 package com.example.twitterdemo;
 
+import android.util.Log;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
 public class UserLogin {
+    private static String loginStatus = "false";
+    private static String message = "Login Failed";
+
+    public static String[] login(String username, String password){
+        ParseUser user = new ParseUser();
+        user.setUsername(username);
+        user.setPassword(password);
+
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e == null) {
+                    Log.i("Logged in...", "Successfully");
+                    loginStatus = "true";
+                } else {
+                    Log.i("Log in...", "Failed" + e.getMessage());
+                    e.printStackTrace();
+                    loginStatus = "false";
+                }
+                message = e.getMessage();
+            }
+        });
+        return new String[] {loginStatus, message};
+    }
 }
