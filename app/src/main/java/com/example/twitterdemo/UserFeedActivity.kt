058e5e69bad10a731d29fe70c17f2896ba26ff4e
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2022-02-07 21:59:31 +0100

    create activity files for displaying user feed

---
+++ app/src/main/java/com/example/twitterdemo/UserFeedActivity.kt
@@ -0,0 +1,11 @@
+package com.example.twitterdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserFeedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_feed)
+    }
+}
