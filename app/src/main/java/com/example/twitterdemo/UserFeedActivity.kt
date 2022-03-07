7b93887434ec8e2b6d66a91e053d84497406cdd5
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2022-03-07 23:59:35 +0100

    get tweet from server and display on user feed view

--- app/src/main/java/com/example/twitterdemo/UserFeedActivity.kt
+++ app/src/main/java/com/example/twitterdemo/UserFeedActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ListView
+import android.widget.SimpleAdapter
 import com.parse.ParseObject
 import com.parse.ParseQuery
 import com.parse.ParseUser
@@ -18,6 +19,7 @@ class UserFeedActivity : AppCompatActivity() {
 
         val userFeedActivity: ListView = findViewById(R.id.userFeedView)
 
+        val tweetData = ArrayList<Map<String, String>>()
 
         val query:ParseQuery<ParseObject> = ParseQuery.getQuery("Tweet")
         query.findInBackground{ objects, e ->
@@ -25,11 +27,23 @@ class UserFeedActivity : AppCompatActivity() {
                 Log.i("Users followed", "Tweet found... Loading to view")
                 for (tweet in objects){
                     Log.i("tweet", tweet.get("tweet").toString())
+
+                    val tweetInfo = HashMap<String, String>()
+                    tweetInfo["content"] = tweet.get("tweet").toString()
+                    tweetInfo["username"] = tweet.get("username").toString()
+                    tweetData.add(tweetInfo)
                 }
+
+                createSimpleAdapter(tweetData)
+
             } else {
                 Log.i("Tweet", "Not found")
 
             }
         }
+    }
+
+    public fun createSimpleAdapter(data:ArrayList<Map<String, String>>): SimpleAdapter{
+        return SimpleAdapter(this, data, android.R.layout.simple_list_item_2, arrayOf("content", "username"), intArrayOf(android.R.id.text1, android.R.id.text2))
     }
 }
