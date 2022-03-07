3c2ea47b5b9d6f41da50f1d71c796f5cbdc4350c
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2022-03-07 23:27:18 +0100

    add ability to get tweet feed from sever

--- app/src/main/java/com/example/twitterdemo/UserFeedActivity.kt
+++ app/src/main/java/com/example/twitterdemo/UserFeedActivity.kt
@@ -2,6 +2,12 @@ package com.example.twitterdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ListView
+import com.parse.ParseObject
+import com.parse.ParseQuery
+import com.parse.ParseUser
 
 class UserFeedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +15,21 @@ class UserFeedActivity : AppCompatActivity() {
         setContentView(R.layout.activity_user_feed)
 
         title = "User feed"
+
+        val userFeedActivity: ListView = findViewById(R.id.userFeedView)
+
+
+        val query:ParseQuery<ParseObject> = ParseQuery.getQuery("Tweet")
+        query.findInBackground{ objects, e ->
+            if (objects!!.isNotEmpty() && e== null){
+                Log.i("Users followed", "Tweet found... Loading to view")
+                for (tweet in objects){
+                    Log.i("tweet", tweet.get("tweet").toString())
+                }
+            } else {
+                Log.i("Tweet", "Not found")
+
+            }
+        }
     }
 }
